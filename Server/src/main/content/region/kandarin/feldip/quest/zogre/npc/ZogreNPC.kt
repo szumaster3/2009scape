69627c977c49a84d452fb1afc9222d1531aae7e6
szumaster3 <szumaster2009@protonmail.com>
2026-04-01 21:47:44 +0200

    Update Zogre npc

--- Server/src/main/content/region/kandarin/feldip/quest/zogre/npc/ZogreNPC.kt
+++ Server/src/main/content/region/kandarin/feldip/quest/zogre/npc/ZogreNPC.kt
@@ -1,13 +1,15 @@
 package content.region.kandarin.feldip.quest.zogre.npc
 
-import core.api.registerTimer
-import core.api.spawnTimer
+import core.api.getOrStartTimer
+import core.api.inEquipment
 import core.game.node.entity.Entity
 import core.game.node.entity.combat.BattleState
+import core.game.node.entity.combat.spell.SpellType
 import core.game.node.entity.npc.NPC
 import core.game.node.entity.npc.NPCBehavior
-import core.tools.RandomFunction
-import core.tools.minutesToTicks
+import core.game.node.entity.player.Player
+import core.game.system.timer.impl.Disease
+import shared.consts.Items
 import shared.consts.NPCs
 
 class ZogreNPC : NPCBehavior(*IDS) {
@@ -19,12 +21,30 @@ class ZogreNPC : NPCBehavior(*IDS) {
         self.task.undead = true
     }
 
-    override fun beforeAttackFinalized(self: NPC, victim: Entity, state: BattleState) {
-        super.beforeAttackFinalized(self, victim, state)
-        if (RandomFunction.roll(10)) {
-            registerTimer(victim, spawnTimer("disease", minutesToTicks(15)))
+    override fun beforeDamageReceived(self: NPC, attacker: Entity, state: BattleState) {
+        if (attacker is Player) {
+            if (inEquipment(attacker, Items.COMP_OGRE_BOW_4827)) {
+                return
+            }
+            if (state.spell != null && state.spell.type == SpellType.CRUMBLE_UNDEAD) {
+                state.estimatedHit = (state.estimatedHit * 0.5).toInt()
+                if (state.secondaryHit > 0) {
+                    state.secondaryHit = (state.secondaryHit * 0.5).toInt()
+                }
+                return
+            }
+            state.estimatedHit = (state.estimatedHit * 0.25).toInt()
+            if (state.secondaryHit > 0) {
+                state.secondaryHit = (state.secondaryHit * 0.25).toInt()
+            }
         }
     }
+
+    override fun beforeAttackFinalized(self: NPC, victim: Entity, state: BattleState) {
+        val disease = getOrStartTimer<Disease>(victim, 10)
+        disease.hitsLeft = 10
+    }
+
 
     companion object {
         private val IDS =
